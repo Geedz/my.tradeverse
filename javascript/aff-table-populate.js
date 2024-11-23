70c9d6ac857c328430d7dce358a7ecdbca61c753
Geedz <95632184+Geedz@users.noreply.github.com>
2024-11-23 18:53:55 +0100

    Add files via upload

--- javascript/aff-table-populate.js
+++ javascript/aff-table-populate.js
@@ -1,6 +1,22 @@
+function getCookie(name) {
+  let nameEQ = name + "=";
+  let cookiesArray = document.cookie.split(';');
+  for (let i = 0; i < cookiesArray.length; i++) {
+      let cookie = cookiesArray[i];
+      while (cookie.charAt(0) === ' ') {
+          cookie = cookie.substring(1, cookie.length);
+      }
+      if (cookie.indexOf(nameEQ) === 0) {
+          return cookie.substring(nameEQ.length, cookie.length);
+      }
+  }
+  return null;
+}
+
+
 document.addEventListener("DOMContentLoaded", async function () {
   // Assume you already have the token stored somewhere, e.g., in localStorage
-  const token = localStorage.getItem("jwt");
+  const token = getCookie("jwt");
   
   // Check if the token exists
   if (!token) {
