89d2f8003ea5ef5f7e2c582a0ee5eeb20a9380a5
Geedz <95632184+Geedz@users.noreply.github.com>
2024-12-04 17:42:31 +0100

    Update video-modal.js

--- javascript/video-modal.js
+++ javascript/video-modal.js
@@ -1,18 +1,21 @@
 video_titles = {
   "vid1": "#1 - Intro | TradeVerse",
   "vid2": "#2 - Cos'é il Trading? | TradeVerse",
-  "vid3": "#3 - Come Funziona la Price Action | TradeVerse"
+  "vid3": "#3 - Come Funziona la Price Action | TradeVerse",
+  "vid4": "#4 - L'Importanza Della Disciplina nel Trading | TradeVerse"
 };
 
 video_urls = {
   "vid1": "https://player.vimeo.com/video/1035034592?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
   "vid2": "https://player.vimeo.com/video/1035799160?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
-  "vid3": "https://player.vimeo.com/video/1035849175?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+  "vid3": "https://player.vimeo.com/video/1035849175?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+  "vid3": "https://player.vimeo.com/video/1036046008?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
 }
 
 const vid1 = document.getElementById("vid1");
 const vid2 = document.getElementById("vid2");
 const vid3 = document.getElementById("vid3");
+const vid3 = document.getElementById("vid4");
 const frame = document.getElementById("video-frame");
 const overlay = document.getElementById("dark-overlay");
 const modal = document.getElementById("frame-container");
@@ -38,6 +41,13 @@ vid3.addEventListener("click", (event) => {
   modalTitle.textContent = video_titles["vid3"];
   frame.style.display = "block";
   frame.setAttribute("src", video_urls["vid3"]);
+})
+
+vid4.addEventListener("click", (event) => {
+  modal.style.display = "block";
+  modalTitle.textContent = video_titles["vid4"];
+  frame.style.display = "block";
+  frame.setAttribute("src", video_urls["vid4"]);
 })
 
 modalClose.addEventListener("click", (event) => {
