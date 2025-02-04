3022ea37fab21cd17be7e0ffae85b446e8a745b6
Geedz <95632184+Geedz@users.noreply.github.com>
2025-02-04 10:01:55 +0100

    Update logout.js

--- javascript/logout.js
+++ javascript/logout.js
@@ -6,6 +6,7 @@ function logout() {
   // Remove the jwt cookie by setting its expiration date to a date in the past.
   // Ensure that the 'path' (and optionally 'domain') match the ones used when the cookie was set.
   document.cookie = "jwt=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=.tradeverse.it;";
+  document.cookie = "current_plan=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=.tradeverse.it;";
 
   // Redirect the user to the login page
   window.location.href = "https://www.tradeverse.it/login.html";
